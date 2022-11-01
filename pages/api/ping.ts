fdaa6ff29ae224089c1f0d3a21789926c39ca67a
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-11-01 13:38:19 +0530

    Fix the hello page creating documents oofff

---
+++ pages/api/ping.ts
@@ -0,0 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+	res.status(200).json({ success: true });
+}
