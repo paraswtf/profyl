cc556fd1a32147f32b3c5446e77ff44267f3da0b
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-11-13 23:28:51 +0530

    Commit for rwlc to review code

--- lib/env.ts
+++ lib/env.ts
@@ -1,7 +1,11 @@
 export const env = {
+	//Uri to mongodb database (mongodb://localhost:27017/database)
 	MONGODB_URI: process.env.MONGODB_URI as string,
+	//Secret for jwt (any string, should not be changed in production)
 	JWT_SECRET: process.env.JWT_SECRET as string,
+	//Base URL where the app is hosted (https://example.com)
 	BASE_URL: process.env.BASE_URL as string,
+	//Api key for sendgrid (https://sendgrid.com/)
 	MAIL_API_KEY: process.env.MAIL_API_KEY as string
 } as const;
 
