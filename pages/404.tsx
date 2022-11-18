4824a487a85d24541ab0eaa9fdf36e89459b84fa
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-11-18 09:19:31 +0530

    It works

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,11 +3,6 @@ import React from "react";
 export default function Invalid() {
 	return (
 		<div className="bg-indigo-900 relative overflow-hidden h-screen">
-			<img
-				alt="404 image"
-				src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
-				className="absolute h-full w-full object-cover"
-			/>
 			<div className="inset-0 bg-black opacity-25 absolute"></div>
 			<div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
 				<div className="w-full font-mono flex flex-col items-center relative z-10">
