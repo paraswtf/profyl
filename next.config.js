ed09b15844a7b428576454bc4ab06b3b2e84b98f
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-12-07 00:53:56 +0530

    Make it a pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPWA = require("next-pwa")({
+	dest: "public/pwa"
+});
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true
+};
+
+module.exports = withPWA({ ...nextConfig });
