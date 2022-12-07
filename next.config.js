0257e4eddf72950e8560914ec0e1420e04db3955
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-12-07 08:47:13 +0530

    Update icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
-	dest: "public/pwa"
+	dest: "public"
 });
 
 const nextConfig = {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,45 +8,41 @@ export default class _Document extends Document {
 
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
 					<link
 						rel="apple-touch-icon"
 						sizes="180x180"
-						href="/icons/apple-touch-icon.png"
+						href="/apple-touch-icon.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="32x32"
-						href="/icons/favicon-32x32.png"
+						href="/favicon-32x32.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="16x16"
-						href="/icons/favicon-16x16.png"
+						href="/favicon-16x16.png"
 					/>
 					<link
 						rel="manifest"
-						href="/icons/site.webmanifest"
+						href="/site.webmanifest"
 					/>
 					<link
 						rel="mask-icon"
-						href="/icons/safari-pinned-tab.svg"
-						color="#228ae6"
-					/>
-					<link
-						rel="shortcut icon"
-						href="/icons/favicon.ico"
+						href="/safari-pinned-tab.svg"
+						color="#4084c5"
 					/>
 					<meta
 						name="msapplication-TileColor"
-						content="#228ae6"
+						content="#1a1b1e"
 					/>
 					<meta
-						name="msapplication-config"
-						content="/icons/browserconfig.xml"
+						name="msapplication-TileImage"
+						content="/mstile-144x144.png"
 					/>
 					<meta
 						name="theme-color"
