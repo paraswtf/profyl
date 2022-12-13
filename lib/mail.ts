e2ad020e1a18a292cc46061c9283d8b82e5b75be
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-12-13 11:26:16 +0530

    Await for vercel coz weird behaviour

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,11 +3,13 @@ import env from "./env";
 
 mail.setApiKey(env.MAIL_API_KEY);
 
-export default function sendVerificationMail(email: string, verificationCode: string, verificationToken: string) {
-	mail.send({
-		from: "short@styxo.codes",
-		to: email,
-		subject: `Verify your account!`,
-		html: `Hello,<br> Please Click on the link or use the code <b>${verificationCode}</b> to verify your email.<br><a href="${env.BASE_URL}/verify?token=${verificationToken}">Click here to verify</a>`
-	}).catch(console.error);
+export default async function sendVerificationMail(email: string, verificationCode: string, verificationToken: string) {
+	await mail
+		.send({
+			from: "short@styxo.codes",
+			to: email,
+			subject: `Verify your account!`,
+			html: `Hello,<br> Please Click on the link or use the code <b>${verificationCode}</b> to verify your email.<br><a href="${env.BASE_URL}/verify?token=${verificationToken}">Click here to verify</a>`
+		})
+		.catch(console.error);
 }
