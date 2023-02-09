d25a23a1e288bd4287f3548d64c7b6f8df31252a
Paras Deshpande <parasbpndeshpande@gmail.com>
2023-02-09 19:28:24 +0530

    Fix env

--- types/env.d.ts
+++ types/env.d.ts
@@ -6,10 +6,8 @@ declare global {
             DATABASE_URL: string;
             //deployment url (https://example.vercel.app)
             NEXT_PUBLIC_HOSTNAME: string;
-            //Uri to mongodb database (mongodb://localhost:27017/database)
-            MONGODB_URI: string;
             //Secret for jwt (any string, should not be changed in production)
-            JWT_SECRET: string;
+            NEXT_SECRET: string;
             //Api key for sendgrid (https://sendgrid.com/)
             MAIL_API_KEY: string;
             //Username for mail account
