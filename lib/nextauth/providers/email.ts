43c5762eca9f2d9e57115122ccbddd104ab9f7e1
Paras Deshpande <parasbpndeshpande@gmail.com>
2023-02-10 12:00:55 +0530

    Fix email image loading

--- lib/nextauth/providers/email.ts
+++ lib/nextauth/providers/email.ts
@@ -45,7 +45,7 @@ export function html(params: { url: string; host: string; theme: Theme }) {
             <tr>
               <td align="center" valign="top" style="padding: 36px 24px;">
                 <a href="${host}" target="_blank" style="display: inline-block;">
-                  <img src="https://${host}/logo.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+                  <img src="https://${host}/emaillogo.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
                 </a>
               </td>
             </tr>
