76cc610c1258d535f899d334429eec7d8a9f87ab
Paras Deshpande <parasbpndeshpande@gmail.com>
2023-02-10 11:46:45 +0530

    Improve email template

--- lib/nextauth/providers/email.ts
+++ lib/nextauth/providers/email.ts
@@ -30,9 +30,34 @@ export function html(params: { url: string; host: string; theme: Theme }) {
     };
 
     return `
-<body style="background: ${color.background};">
+<body style="background: ${color.background}; padding-top: 50px; padding-bottom: 50px;">
   <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; margin-top: 50px; margin-bottom: 50px; border-radius: 10px;">
+    <!-- start logo -->
+      <tr>
+        <td align="center" bgcolor="#e9ecef">
+          <!--[if (gte mso 9)|(IE)]>
+          <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
+            <tr>
+              <td align="center" valign="top" width="600">
+          <![endif]-->
+          <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+            <tr>
+              <td align="center" valign="top" style="padding: 36px 24px;">
+                <a href="${host}" target="_blank" style="display: inline-block;">
+                  <img src="https://${host}/logo.svg" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+                </a>
+              </td>
+            </tr>
+          </table>
+          <!--[if (gte mso 9)|(IE)]>
+              </td>
+            </tr>
+          </table>
+        <![endif]-->
+        </td>
+      </tr>
+      <!-- end logo -->
     <tr>
       <td align="center"
         style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
