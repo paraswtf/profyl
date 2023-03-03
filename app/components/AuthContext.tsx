e10efeb4443e07b42455540cc1293d835f745a84
Paras Deshpande <parasbpndeshpande@gmail.com>
2023-03-03 15:13:14 +0530

    Faster loading using swr

--- app/components/AuthContext.tsx
+++ app/components/AuthContext.tsx
@@ -2,12 +2,33 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
+import useSWR from 'swr';
 
 export interface AuthContextProps {
     children: React.ReactNode;
-    session: Session;
+    cookie: string;
 }
 
-export default function AuthContext({ session, children }: AuthContextProps) {
+async function getSession(cookie: string): Promise<Session> {
+    const response = await fetch(
+        `http${
+            process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ? '' : 's'
+        }://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/session`,
+        {
+            headers: {
+                cookie,
+            },
+        }
+    );
+
+    const session = await response.json();
+
+    return Object.keys(session).length > 0 ? session : null;
+}
+
+export default function AuthContext({ cookie, children }: AuthContextProps) {
+    const { data: session } = useSWR('/api/auth/session', () =>
+        getSession(cookie)
+    );
     return <SessionProvider session={session}>{children}</SessionProvider>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar';
 import RootStyleRegistry from './emotion';
 import { Noto_Sans } from 'next/font/google';
-import { Session } from 'next-auth';
 import { headers } from 'next/headers';
 import AuthContext from './components/AuthContext';
 
@@ -18,35 +17,17 @@ const noto = Noto_Sans({
     subsets: ['latin'],
 });
 
-async function getSession(cookie: string): Promise<Session> {
-    const response = await fetch(
-        `http${
-            process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ? '' : 's'
-        }://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/session`,
-        {
-            headers: {
-                cookie,
-            },
-        }
-    );
-
-    const session = await response.json();
-
-    return Object.keys(session).length > 0 ? session : null;
-}
-
 export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const session = await getSession(headers().get('cookie') ?? '');
     return (
         <html lang="en-US">
             <head />
             <body className={noto.className}>
                 <RootStyleRegistry>
-                    <AuthContext session={session}>
+                    <AuthContext cookie={headers().get('cookie') ?? ''}>
                         <Navbar />
                         {children}
                     </AuthContext>
