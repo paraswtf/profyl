5cc44d791a39aab4d0b0a40215e9b0acff016e69
Paras Deshpande <parasbpndeshpande@gmail.com>
2023-03-25 12:03:53 +0530

    UI update (dont push to main)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Head from 'next/head';
 import Test from './test-component';
 
 export default function Page() {
-    return <Test />;
+    return (
+        <>
+            <Head>
+                <title key="home">Profyl</title>
+            </Head>
+            <Test />
+        </>
+    );
 }
