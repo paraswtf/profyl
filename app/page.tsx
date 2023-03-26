9d84ea451f3bc9eff2c03e12ef424e7381330534
Paras Deshpande <parasbpndeshpande@gmail.com>
2023-03-26 12:01:17 +0530

    Add confetti

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Test from './test-component';
 
 export default function Page() {
     return (
-        <>
+        <div style={{ overflowX: 'hidden' }}>
             <Head>
                 <title key="home">Profyl</title>
             </Head>
             <Test />
-        </>
+        </div>
     );
 }
